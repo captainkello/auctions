c94e150b6dc1cc87fedb1c5604d4a42edb426eae
Ganesh <ganeshprasadsr@gmail.com>
2010-10-25 08:04:06 +0530

    initial copy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auctions::Application.config.secret_token = '8ca751798146656dce266f0da31576a8bc1f17767199da3d0bb7fbcb02fdd6482f9a6e266fed5e1f971525e767cbfdb816dbfc8e1571ec82c802218201af9044'
